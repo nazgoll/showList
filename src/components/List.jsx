5c7bf5f52e35c5c407f3c22b819ffe9c687f8703
nazgol <nazgool.momeni1@gmail.com>
2023-07-25 23:36:11 +0330

    start

---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+const List = (props) => {
+    return (
+
+        <div className={"w-3/5 h-3/5 p-10 bg-opacity-60 bg-custom-color-2 backdrop-filter flex shadow-black shadow-lg  flex-col  items-center p-10" +
+            "backdrop-blur-lg shadow-lg rounded-lg "}>
+        </div>
+
+    )
+}
+export default List;
