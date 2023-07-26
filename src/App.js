11b5d4da8c777fc2c18b15e77d2446419f620237
nazgol <nazgool.momeni1@gmail.com>
2023-07-26 13:47:22 +0330

    start 2

--- src/App.js
+++ src/App.js
@@ -3,30 +3,46 @@ import Header from "./components/Header";
 import Box from "./components/Box";
 import List from "./components/List";
 
-function App() {
-    const [contact,setContact]=useState(null)
+function App=() =>{
+    const [information, setInformation] = useState(null);
+    const handleInformation(list)=>{
+        setContact(list)
+    }
+    const info=[
+        {name:"nazgol",family:"Momeni"},
+        {name:"mehrana",family:"Sadeghi"},
+        {name:"zahra",family:"Mosavi"},
+
+    ];
+
     return (
         <div className={"w-screen h-screen flex flex-row bg-white "}>
             <div className={"basis-3/4 h-full border-r-custom-color-4 border-r-4 flex flex-col items-center "}>
                 <Header headerText={"ShowList"}/>
-                <div
-                    className={"w-3/5 h-3/5 p-10 bg-opacity-60 bg-custom-color-2 backdrop-filter flex shadow-black shadow-lg  flex-col  items-center p-10" +
+                <div className={"w-3/5 h-3/5 p-10 bg-opacity-60 bg-custom-color-2 backdrop-filter flex shadow-black shadow-lg  flex-col  items-center p-10" +
                         "backdrop-blur-lg shadow-lg rounded-lg "}>
+                        {info.map((contact)=>(
+                            <List
+                            name={contact.name}
+                            family={contact.family}
+                            onClick={(contact)=>handleInformation(contact)}
+                            />
+                            ))}
                     <div className={"bg-custom-color-1 my-2 mt-14 rounded-lg shadow-black shadow-lg  flex-col justify-center items-center inline-flex hover:bg-custom-color-4 hover:text-custom-color-1 w-36"}
-                    onClick={<List/>}>
+                        onClick={<List/>}>
 
                     </div>
                 </div>
             </div>
-
+            const Order = ({ title, item }) => (
             <div className={"basis-1/4 h-full bg-custom-color-2 flex flex-col items-center "}>
                 <Header headerText={"Members"}></Header>
-                <button onClick={""}><Box headerText={"Nazgol"}/></button>
-                <Box headerText={"Mehrana"}/>
-                <Box headerText={"Sajedeh"}/>
-
+                {/*<button onClick={"nazgol"}><Box headerText={"Nazgol"}/></button>*/}
+                {/*<Box headerText={"Mehrana"}/>*/}
+                {/*<Box headerText={"zahra"}/>*/}
+                {}
             </div>
-
+            )
         </div>
     )
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,21 @@
 const List = (props) => {
 
-    const Info= ()=> [
-        {"name":"nazgol","family":"momeni","age":22 }
-    ]
+    // const Info= ()=> [
+    //     {"name":"nazgol","family":"momeni","age":22 }
+    // ]
+    const info={
+        name: "n", family:"m"
+    }
     return (
 
-        <div
-            className={"w-3/5 h-3/5 p-10 bg-opacity-60 bg-custom-color-2 backdrop-filter flex shadow-black shadow-lg  flex-col  items-center p-10" +
-                "backdrop-blur-lg shadow-lg rounded-lg "}>
-
-        </div>
-
+        // <div
+        //     className={"w-3/5 h-3/5 p-10 bg-opacity-60 bg-custom-color-2 backdrop-filter flex shadow-black shadow-lg  flex-col  items-center p-10" +
+        //         "backdrop-blur-lg shadow-lg rounded-lg "}>
+        //
+        // </div>
+<div className={"bg-withe"}>
+    <h2>{props.name}</h2>
+</div>
     )
 }
 export default List;
